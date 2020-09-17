c86838db5ebd0d4a24cebf0661007ae4a76689a0
asecoder3 <54946934+asecoder3@users.noreply.github.com>
2020-09-17 18:16:16 +0300

    muokkasin

--- main.lua
+++ main.lua
@@ -1,10 +1,34 @@
 -- pöö
 function love.load()
-	leveys = love.graphics.getWidth()
-	korkeus = love.graphics.getHeight()
+	leveys = 20
+	korkeus = 15
+	
+	reset()
+end
+
+function reset ()
+
+	mato = {
+	{x = 3, Y = 1},
+	{x = 2, Y = 1},
+	{x = 1, Y = 1},
+	}
+	
+	
+	suunta = 'ringht'
+	ajastin = 0
+	
 end
 
 function love.draw()
-	teksti = "Hello World"
-	love.graphics.print(teksti,leveys/2, korkeus/2)
+
+	local ruutu = 15
+	
+	love.graphics.setColor(.28,.28,.28)
+	
+	love.graphics.rectangle('fill',0, 0,leveys*ruutu,korkeus*ruutu)
+	
+	local function piirraRuutu(x,y)
+		love.graphics.rectangle('fill',(x-1)*ruutu,(y-1) * ruutu, ruutu, -1, ruutu-1)
+	end
 end
